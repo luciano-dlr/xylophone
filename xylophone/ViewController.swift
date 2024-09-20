acfd3504bae64bb1e20221ec500128d792188769
luciano-dlr <delarubialuciano@gmail.com>
2024-09-20 18:07:31 -0300

    Initial Commit

---
+++ xylophone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xylophone
+//
+//  Created by Luciano de la Rubia on 20/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ xylophoneTests/xylophoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  xylophoneTests.swift
+//  xylophoneTests
+//
+//  Created by Luciano de la Rubia on 20/09/2024.
+//
+
+import Testing
+@testable import xylophone
+
+struct xylophoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
