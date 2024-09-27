404176c24a82ee434375834f2e31f2c8637efd53
luciano-dlr <delarubialuciano@gmail.com>
2024-09-26 21:47:29 -0300

    feature(btnSounds): Make sounds pair with label on interface

--- xylophone/ViewController.swift
+++ xylophone/ViewController.swift
@@ -4,16 +4,28 @@
 //
 //  Created by Luciano de la Rubia on 20/09/2024.
 //
-
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+//        playSound()
+//        print(sender.currentTitle)
+        
+        playSound(titeButton: sender.currentTitle ?? "")
+    }
+    
+    func playSound(titeButton: String) {
+        let url = Bundle.main.url(forResource: titeButton, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
-
